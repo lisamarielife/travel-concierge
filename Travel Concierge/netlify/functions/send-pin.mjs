bb860d1a355ff3c0485e8d122175d5f112ee0da7
lisamarielife <lisamarieaicoach@gmail.com>
2026-09-09 17:45:37 +1000

    Update send-pin.mjs

--- Travel Concierge/netlify/functions/send-pin.mjs
+++ Travel Concierge/netlify/functions/send-pin.mjs
@@ -51,7 +51,7 @@ export default async (req, context) => {
     if (!isPaid) {
       return new Response(
         JSON.stringify({
-          message: "Email not recognised. Please double-check that you're using the same email address you used at purchase checkout. Still stuck? Contact lisamarieaicoach.com",
+          message: "Email not recognised. Please double-check that you're using the same email address you used to purchase. Still stuck? Contact lisamarieaicoach.com",
         }),
         {
           status: 403,
